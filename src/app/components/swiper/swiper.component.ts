8af53231d7d3d902c83e10ac2c8fdb56c5e3a137
jian玄冰 <jianxuanhuo1@126.com>
2017-06-12 18:05:14 +0800

    OK

--- src/app/components/swiper/swiper.component.ts
+++ src/app/components/swiper/swiper.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 @Component({
     selector:'bing-swiper',
-    templateUrl:''
+    templateUrl:'./swiper.component.html',
+    styleUrls:['./swiper.component.scss']
 })
